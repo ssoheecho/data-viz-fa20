d2e296e051e85ecfba8d322dbca0ba4e284dff76
Sohee Cho <soheecho94@gmail.com>
2020-10-20 18:15:45 -0400

    assignment 2

---
+++ assignment_02_03/README.md
@@ -0,0 +1,6 @@
+Read full documentation on the assignment [here](https://www.notion.so/assignment-2-3-5b3d37be3e46464692fd87ca3d43e1a0)
+
+### exercise
+
+Choose a sample dataset. Ask three questions of your data. The questions should be a series of questions that move from general to specific, but the can be three completely different questions. Each chart can be visualized in any way you would like in any medium. Try and incorporate more than one encoding discussed in the lecture.
+
---
+++ assignment_02_03/bargraph.js
@@ -0,0 +1,83 @@
+// basic method with d3 is to select an element and append to it
+// since we don't have anything in the body yet,
+// we're going to select the body and append our svg element
+// and save it in a variable
+const svg = d3.select("body").append("svg")
+.attr("height", "100%") // the svg element comes in a default, so we are going to change the size here
+.attr("width", "100%") // setting the height & weight to be at 100%
+
+var nameData = [];
+
+d3.csv("cereal.csv").then(function(data) {
+  data.forEach(d => (nameData.push(d.name)))
+  //console.log(nameData)
+
+   // scales:
+  let height = 300;
+
+  let margin = {
+                left: [50, 55],
+                right: 50,
+                top: 100,
+                bottom: 0 
+              }
+
+  // axis
+  let y = d3.scaleLinear()
+            .domain([0, 160])
+            .range([height, 0]);
+
+  let yAxis = d3.axisLeft(y).tickPadding(10);
+
+  let tooltip = d3.select("body")
+                  .append("div")
+                  .style("position", "absolute")
+                  .style("z-index", "10")
+                  .style("visibility", "hidden")
+                  .style("color", "white")
+                  .style("background-color", "gray")
+                  .style("border-radius", "5px")
+                  .style("padding", "5px")
+                  .text("a simple tooltip");
+
+var mousemove = function(d) {
+  tooltip
+    .html("The exact value of<br>this cell is: " + d.value)
+    .style("left", (d3.mouse(this)[0]+70) + "px")
+    .style("top", (d3.mouse(this)[1]) + "px")
+}
+  
+  let chartGroup = svg.append("g").attr("transform", "translate("+margin.left[1]+", "+margin.top+")");
+
+  chartGroup.selectAll("rect") // rect doesn't exist yet
+    .data(data) // binding data
+    .enter() // where all the data that wasn't bound properly go into
+    .append("rect") // creating/appending our rectangles
+    .attr("height", (d) => height - y(parseInt(d.calories))) // binding data to make dynamic bars
+    .attr("width", "6")
+    .attr("x", (d, i) =>  i * 8 )
+    .attr("y", (d) => y(parseInt(d.calories)))
+    .attr("fill", "#8859b6")
+    .on("mouseover", function(){
+      return tooltip.data(data)
+                    .style("visibility", "visible")
+                    .text((d) => d.name + " - " + d.calories + " cal");
+    })
+	  .on("mousemove", function(){
+      return tooltip.style("top", (event.pageY-40)+"px")
+                    .style("left",(event.pageX-60)+"px")
+    })
+	  .on("mouseout", function(){
+      return tooltip.style("visibility", "hidden");
+    });
+
+  chartGroup.append("g")
+    .attr("class", "axis")
+    .call(yAxis)
+
+  chartGroup.append("text")
+    .attr("x", 300)             
+    .attr("y", 350)
+    .attr("text-anchor", "middle")  
+    .text("calories per cereal");
+})
